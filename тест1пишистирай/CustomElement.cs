a3d34b6f653897ee948069bcbe6094ea6a20ed0d
furuh <furuh@DESKTOP-TL7NP8K>
2022-05-25 19:54:03 +0500

    Добавьте файлы проекта.

---
+++ тест1пишистирай/CustomElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace тест1пишистирай
+{
+    public partial class CustomElement : UserControl
+    {
+        private string _name;
+        private string _description;
+        private string _manufacturer;
+        private string _cost;
+        private string _discount;
+        private Image _iamge;
+
+        public Image Image
+        {
+            get { return _iamge; }
+            set { _iamge = value; pbImage.Image = value; }
+        }
+        public string Discount
+        {
+            get { return _discount; }
+            set { _discount = value; lDiscount.Text += $" {value}"; }
+        }
+        public string Cost
+        {
+            get { return _cost; }
+            set { _cost = value; lCost.Text = value; }
+        }
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+            set { _manufacturer = value; lManufacturer.Text += $" {value}"; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; lDescription.Text += $" {value}"; }
+        }
+        public string PName
+        {
+            get { return _name; }
+            set { _name = value; lName.Text = value; }
+        }
+        public CustomElement()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ тест1пишистирай/DataBank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тест1пишистирай
+{
+    public static class DataBank
+    {
+        public static SqlConnection Connection = new SqlConnection(@"Data Source=DESKTOP-TL7NP8K\SQLEXPRESS;Initial Catalog=Тест1;Integrated Security=True");
+    }
+}
